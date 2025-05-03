6cc4fac7d81ca2448788e112fcf95b1db0dd55a6
Gabriela Shirley <gabrielashirleyh@gmail.com>
2025-05-03 14:01:31 -0400

    Fix lint build errors

--- src/database/schema/auth.ts
+++ src/database/schema/auth.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, integer, timestamp, boolean } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, boolean } from "drizzle-orm/pg-core";
 			
 export const users = pgTable("users", {
 					id: text("id").primaryKey(),
